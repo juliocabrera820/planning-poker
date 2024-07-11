d9f38242f51675cd42975a93c8866df38be05cfe
juliocabrera820 <juliocabrera820@gmail.com>
2024-07-11 13:09:42 -0600

    generate room

--- lib/planning_poker_web/live/room_live/index.ex
+++ lib/planning_poker_web/live/room_live/index.ex
@@ -6,15 +6,20 @@ defmodule PlanningPokerWeb.RoomLive.Index do
     socket =
       socket
       |> assign(:session, nil)
+      |> assign(:url, nil)
 
     {:ok, socket}
   end
 
   @impl true
   def handle_event("submit-session", %{"session" => session}, socket) do
+    slug = Slug.slugify(session, lower: true)
+    session_url = url(~p"/room/planning/#{slug}")
+
     socket =
       socket
       |> assign(:session, session)
+      |> assign(:url, session_url)
 
     {:noreply, socket}
   end
